e26299f8c716db150e82cdb62f53d4869e2c39db
Bernardo Vale <gerrardpardie@gmail.com>
2012-12-24 17:01:36 -0200

    Feito a busca de disciplinas por prof

---
+++ src/br/edu/utfpr/model/managedBeans/VisualizarProfessoresBean.java
@@ -0,0 +1,54 @@
+package br.edu.utfpr.model.managedBeans;
+
+import br.edu.utfpr.model.dao.DisciplinaDao;
+import br.edu.utfpr.model.dao.ProfessorDao;
+import br.edu.utfpr.model.pojo.Disciplina;
+import br.edu.utfpr.model.pojo.Professor;
+
+import javax.faces.model.SelectItem;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Bernardo Vale
+ *         Date: 24/12/12
+ *         Time: 16:12
+ */
+public class VisualizarProfessoresBean {
+
+    private DisciplinaDao disciplinaDao = new DisciplinaDao();
+    private ProfessorDao professorDao = new ProfessorDao();
+    private Long currentProfessor = 0L;
+    private List<SelectItem> professorList = getProfessores();
+
+    public void buscar(){
+        List<Disciplina> disciplinas = professorDao.findByID(currentProfessor).getDisciplinasMinistrantes();
+        for(Disciplina d : disciplinas){
+            System.out.printf(d.getNome());
+        }
+        System.out.println(currentProfessor);
+    }
+
+    public void salvar(){
+        System.out.println("caca");
+    }
+    public Long getCurrentProfessor() {
+        return currentProfessor;
+    }
+
+    public void setCurrentProfessor(Long currentProfessor) {
+        this.currentProfessor = currentProfessor;
+    }
+
+    public List<SelectItem> getProfessorList() {
+        return professorList;
+    }
+
+    public List<SelectItem> getProfessores() {
+        List<SelectItem> professores = new ArrayList<SelectItem>();
+        for(Professor p : professorDao.findAll()){
+            professores.add(new SelectItem(p.getId(),p.getNome()));
+        }
+        return professores;
+    }
+}
